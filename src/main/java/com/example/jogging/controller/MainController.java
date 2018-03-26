dcf20a6397af7fabe7be262e1227b37a221ff5ef
Wiktor89 <afonin.vitia2000@yandex.ru>
2018-03-26 19:15:42 +0400

    REST API

---
+++ src/main/java/com/example/jogging/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.example.jogging.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by VAfonin on 26.03.2018.
+ */
+@RestController
+@RequestMapping(value = "/main")
+public class MainController {
+
+    private static final String ADD_USERS = "/add_users";
+
+    @RequestMapping(value = MainController.ADD_USERS, method = RequestMethod.GET)
+    public void test () {
+        System.out.println ();
+    }
+}
