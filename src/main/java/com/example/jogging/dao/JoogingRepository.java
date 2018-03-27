284880b65f126eb74b9bd3adf677d8f3a20bbe35
Wiktor89 <afonin.vitia2000@yandex.ru>
2018-03-27 12:58:52 +0400

    add DAO

---
+++ src/main/java/com/example/jogging/dao/JoogingRepository.java
@@ -0,0 +1,12 @@
+package com.example.jogging.dao;
+
+import com.example.jogging.models.Jooging;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by VAfonin on 27.03.2018.
+ */
+@Repository
+public interface JoogingRepository extends JpaRepository<Jooging, Integer> {
+}
